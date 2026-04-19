a7a9bc55ce1cfeccf7d8476a367fb612a5aee6f0
ZakariGames <133550763+ZakariGames@users.noreply.github.com>
2026-04-19 10:08:49 -0400

    Sound Effects!

--- scripts/audio_manager.gd
+++ scripts/audio_manager.gd
@@ -1,8 +1,17 @@
 extends Node
 
 # add onreadys for each sound file
-@onready var sfx_button: AudioStreamPlayer = $SfxButton
 @onready var sfx_engine_idle: AudioStreamPlayer = $SfxEngineIdle
 @onready var sfx_engine_rev_down: AudioStreamPlayer = $SfxEngineRevDown
 @onready var sfx_engine_rev_up: AudioStreamPlayer = $SfxEngineRevUp
 @onready var sfx_engine_running: AudioStreamPlayer = $SfxEngineRunning
+
+@onready var sfx_button: AudioStreamPlayer = $SfxButton
+@onready var sfx_sensor_pickup: AudioStreamPlayer = $SfxSensorPickup
+
+@onready var ui_cancel: AudioStreamPlayer = $UiCancel
+@onready var ui_click: AudioStreamPlayer = $UiClick
+@onready var ui_preview: AudioStreamPlayer = $UiPreview
+@onready var ui_reset: AudioStreamPlayer = $UiReset
+@onready var ui_rollout: AudioStreamPlayer = $UiRollout
+@onready var ui_storm: AudioStreamPlayer = $UiStorm
